0d8076c5afad77ded50310f0579527a55540511b
Dennis Macharia <dennismacharia4747@gmail.com>
2019-02-28 09:57:08 +0300

    handle callbacks

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,8 +1,26 @@
+/* eslint-disable camelcase */
 import express from 'express';
 import axios from 'axios';
+import request from 'request';
 import querystring from 'querystring';
 
 const router = new express.Router();
+
+function sendMessageToSlackResponseURL(responseURL, JSONmessage) {
+  const postOptions = {
+    uri: responseURL,
+    method: 'POST',
+    headers: {
+      'Content-type': 'application/json',
+    },
+    json: JSONmessage,
+  };
+  request(postOptions, (error) => {
+    if (error) {
+      console.log(error);
+    }
+  });
+}
 
 const sendDirectMessageTobot = (responseText) => {
   const options = {
@@ -64,6 +82,210 @@ router.post('/events', (req, res) => {
       response = 'Hello there :smiley: \n What have you accomplished in the past 24hrs?';
       sendDirectMessageTobot(response);
     }
+  }
+});
+
+router.post('/command/report', (req, res) => {
+  res.status(200).end();
+  const payload = req.body;
+  const responseURL = payload.response_url;
+
+  const feedback = {
+    text: 'Hello :smiley:',
+    attachments: [
+      {
+        text: 'What\'s your progress in the past 24hrs?',
+        fallback: "Shame... buttons aren't supported in this land",
+        callback_id: 'progress',
+        color: '#3AA3E3',
+        attachment_type: 'default',
+        actions: [
+          {
+            name: 'Productive',
+            text: 'Productive',
+            type: 'button',
+            value: 'yes',
+          },
+          {
+            name: 'Unproductive',
+            text: 'Unproductive',
+            type: 'button',
+            value: 'no',
+            style: 'danger',
+          },
+          {
+            name: 'On vacation',
+            text: 'On vacation',
+            type: 'button',
+            value: 'maybe',
+          },
+        ],
+      },
+    ],
+  };
+  sendMessageToSlackResponseURL(responseURL, feedback);
+});
+
+router.post('/actions', (req, res) => {
+  try {
+    const actionJSONPayload = JSON.parse(req.body.payload); // parse URL-encoded payload JSON string
+    const { callback_id } = actionJSONPayload;
+    let message;
+    switch (callback_id) {
+    case 'progress':
+      res.status(200).end(); // best practice to respond with 200 status
+      console.log(actionJSONPayload);
+      if (actionJSONPayload.actions[0].name === 'Productive') {
+        message = {
+          text: 'That\'s great :clap:',
+          attachments: [
+            {
+              text: 'Is your progress demoable',
+              replace_original: false,
+              fallback: "Shame... buttons aren't supported in this land",
+              callback_id: 'feedback',
+              color: '#3AA3E3',
+              attachment_type: 'default',
+              actions: [
+                {
+                  name: 'Yes',
+                  text: 'Yes',
+                  type: 'button',
+                  value: 'yes',
+                },
+                {
+                  name: 'No',
+                  text: 'No',
+                  type: 'button',
+                  value: 'no',
+                },
+              ],
+            },
+          ],
+        };
+        sendMessageToSlackResponseURL(actionJSONPayload.response_url, message);
+      } if (actionJSONPayload.actions[0].name === 'Unproductive') {
+        message = {
+          text: 'That\'s okay.',
+          attachments: [
+            {
+              text: 'Do you have any blockers',
+              replace_original: false,
+              fallback: "Shame... buttons aren't supported in this land",
+              callback_id: 'feedback',
+              color: '#3AA3E3',
+              attachment_type: 'default',
+              actions: [
+                {
+                  name: 'Yes',
+                  text: 'Yes',
+                  type: 'button',
+                  value: 'yes',
+                },
+                {
+                  name: 'No',
+                  text: 'No',
+                  type: 'button',
+                  value: 'no',
+                },
+              ],
+            },
+          ],
+        };
+        sendMessageToSlackResponseURL(actionJSONPayload.response_url, message);
+      } if (actionJSONPayload.actions[0].name === 'On vacation') {
+        message = {
+          text: 'Happy holidays.',
+        };
+        sendMessageToSlackResponseURL(actionJSONPayload.response_url, message);
+      }
+      break;
+
+    case 'feedback':
+      res.status(200).end();
+      if (actionJSONPayload.actions[0].name === 'Yes') {
+        message = {
+          text: 'Awesome :+1:',
+          attachments: [
+            {
+              text: 'Have you updated your client on this',
+              replace_original: false,
+              fallback: "Shame... buttons aren't supported in this land",
+              callback_id: 'feedback2',
+              color: '#3AA3E3',
+              attachment_type: 'default',
+              actions: [
+                {
+                  name: 'Yes',
+                  text: 'Yes',
+                  type: 'button',
+                  value: 'yes',
+                },
+                {
+                  name: 'No',
+                  text: 'No',
+                  type: 'button',
+                  value: 'no',
+                },
+              ],
+            },
+          ],
+        };
+        sendMessageToSlackResponseURL(actionJSONPayload.response_url, message);
+      } if (actionJSONPayload.actions[0].name === 'No') {
+        message = {
+          text: 'That\'s cool',
+          attachments: [
+            {
+              text: 'Have you updated your client on this',
+              replace_original: false,
+              fallback: "Shame... buttons aren't supported in this land",
+              callback_id: 'feedback2',
+              color: '#3AA3E3',
+              attachment_type: 'default',
+              actions: [
+                {
+                  name: 'Yes',
+                  text: 'Yes',
+                  type: 'button',
+                  value: 'yes',
+                },
+                {
+                  name: 'No',
+                  text: 'No',
+                  type: 'button',
+                  value: 'no',
+                },
+              ],
+            },
+          ],
+        };
+        sendMessageToSlackResponseURL(actionJSONPayload.response_url, message);
+      }
+
+      break;
+
+    case 'feedback2':
+      res.status(200).end();
+      if (actionJSONPayload.actions[0].name === 'Yes') {
+        message = {
+          text: 'Keep it up. \n Continue with the same spirit. Communication is a powerfull tool. ',
+        };
+        sendMessageToSlackResponseURL(actionJSONPayload.response_url, message);
+      } if (actionJSONPayload.actions[0].name === 'No') {
+        message = {
+          text: 'It is a good practice to keep the client on the loop. Communication is key. ',
+        };
+        sendMessageToSlackResponseURL(actionJSONPayload.response_url, message);
+      }
+
+      break;
+
+    default:
+      console.log('Ooops! Something went wrong');
+    }
+  } catch (error) {
+    console.log('Ooops! Something went extra extra wrong');
   }
 });
 
